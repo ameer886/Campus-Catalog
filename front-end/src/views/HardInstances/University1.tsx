6d568d01ff2933331afc15aea38e2e559bd6bc58
RG8452 <ryangahagan18@gmail.com>
2021-09-29 21:06:04 -0500

    Add in hard-coded example instances to be removed later

---
+++ front-end/src/views/HardInstances/University1.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import University from '../../components/University/University';
+
+/*
+ * Hard-code a university here
+ */
+const University1: React.FunctionComponent = () => {
+  return <University id={1} name="Example University 1" />;
+};
+
+export default University1;
---
+++ front-end/src/views/HardInstances/University2.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import University from '../../components/University/University';
+
+/*
+ * Hard-code a university here
+ */
+const University2: React.FunctionComponent = () => {
+  return <University id={2} name="Example University 2" />;
+};
+
+export default University2;
