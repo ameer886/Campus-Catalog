1fec47a7af3e4c924886d06b74e5fc742930f80a
RG8452 <ryangahagan18@gmail.com>
2021-10-04 12:31:01 -0500

    Style splash page with grid

---
+++ front-end/src/views/SplashPage/SplashGrid.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import styles from './SplashGrid.module.css';
+
+import ApartmentImg from './Images/Apartment.png';
+import HarvardImg from './Images/Harvard.jpeg';
+import EntertainmentImg from './Images/Entertainment.jpeg';
+import { Nav } from 'react-bootstrap';
+
+type CardProps = {
+  href: string;
+  title: string;
+  img: string;
+};
+
+const allCards: CardProps[] = [
+  {
+    href: '/entertainments',
+    title: 'Entertainment',
+    img: EntertainmentImg,
+  },
+  {
+    href: '/universities',
+    title: 'University',
+    img: HarvardImg,
+  },
+  {
+    href: '/apartments',
+    title: 'Apartments',
+    img: ApartmentImg,
+  },
+];
+
+type SplashCardProps = {
+  card: CardProps;
+};
+
+const SplashCard: React.FunctionComponent<SplashCardProps> = ({
+  card,
+}: SplashCardProps) => {
+  return (
+    <Nav.Link href={card.href}>
+      <div className={styles.SplashCardContainer}>
+        <span className={styles.CardLabel}>{card.title}</span>
+        <div className={styles.SplashCard}>
+          <img
+            className={styles.SplashImg}
+            src={card.img}
+            alt={card.title + ' img'}
+          />
+        </div>
+      </div>
+    </Nav.Link>
+  );
+};
+
+/*
+ * A grid of 3 cards
+ * Each card will link to one models page
+ */
+const SplashGrid: React.FunctionComponent = () => {
+  return (
+    <div className={styles.Centering}>
+      <div className={styles.SplashGridContainer}>
+        {allCards.map((card, index) => (
+          <SplashCard card={card} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SplashGrid;
