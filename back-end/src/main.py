c13b71038e2f4fd8a8ec3a99f03502b981b099a4
Huan He <ameer886@outlook.com>
2021-10-19 12:32:50 -0700

    added sample home routing

--- back-end/src/main.py
+++ back-end/src/main.py
@@ -1,5 +1,13 @@
 from flask import Flask
-from models import app, University
+from db import db_init
+from models import University, Housing, Amenities
+
+app = Flask(__name__)
+db = db_init(app)
+
+@app.route('/')
+def home():
+    return 'hello world'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
