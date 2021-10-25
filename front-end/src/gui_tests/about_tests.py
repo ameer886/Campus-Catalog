096a7e95c086d40b44b8485226a341431630d3c9
Brandon Hinh <brandonhinh@utexas.edu>
2021-10-25 15:50:45 -0500

    Add chromedrivers and implement navbar and about page selenium tests

---
+++ front-end/src/gui_tests/about_tests.py
@@ -0,0 +1,51 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import sys
+
+URL = "http://localhost:3000/about/"
+
+class Test(unittest.TestCase):
+   @classmethod
+   def setUpClass(cls):
+      chrome_options = Options()
+      chrome_options.add_argument('--no-sandbox')
+      chrome_options.add_argument('--headless')
+      chrome_options.add_argument('--disable-dev-shm-usage')
+      chrome_options.add_argument("--disable-extensions")
+      cls.driver = webdriver.Chrome(PATH, options=chrome_options)
+      cls.driver.get(URL)
+
+   @classmethod
+   def tearDownClass(cls):
+      cls.driver.quit()
+   
+   def testAboutInformation(self):
+      self.driver.get(URL)
+      self.driver.implicitly_wait(10)
+      assert self.driver.title == "Campus Catalog"
+      title = self.driver.find_element_by_class_name('AboutPage_Title__2srkJ').text
+      assert title == 'About'
+      subheaders = ['Our Mission', 'Why Use Our Website?', 'Us', 'Our Codebase', 'Our Tools', 'Our APIs']
+      elems = self.driver.find_elements_by_class_name('AboutPage_Section__1c-dF')
+      assert [x.text for x in elems] == subheaders
+   
+   def testCodebaseLinks(self):
+      self.driver.get(URL)
+      self.driver.implicitly_wait(10)
+
+      link = self.driver.find_element_by_link_text('Our Postman docs').get_attribute('href')
+      assert link == 'https://documenter.getpostman.com/view/17627995/UUy3A7Rd'
+
+      self.driver.back()
+      self.driver.implicitly_wait(10)
+      link = self.driver.find_element_by_link_text('Our GitLab repository').get_attribute('href')
+      assert link == 'https://gitlab.com/RG8452/campus-catalog/'
+
+      
+      
+
+
+if __name__ == "__main__":
+    PATH = sys.argv[1]
+    unittest.main(argv=['first-arg-is-ignored'])
---
+++ front-end/src/gui_tests/navbar_tests.py
@@ -0,0 +1,83 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import sys
+
+URL = "http://localhost:3000/"
+
+class Test(unittest.TestCase):
+   @classmethod
+   def setUpClass(cls):
+      chrome_options = Options()
+      chrome_options.add_argument('--no-sandbox')
+      chrome_options.add_argument('--headless')
+      chrome_options.add_argument('--disable-dev-shm-usage')
+      chrome_options.add_argument("--disable-extensions")
+      cls.driver = webdriver.Chrome(PATH, options=chrome_options)
+      cls.driver.get(URL)
+
+   @classmethod
+   def tearDownClass(cls):
+      cls.driver.quit()
+   
+   def testHome(self):
+      self.driver.get(URL)
+      self.driver.implicitly_wait(10)
+      self.driver.find_element_by_link_text('Campus Catalog').click()
+      assert self.driver.title == "Campus Catalog"
+      assert URL == self.driver.current_url
+      text = self.driver.find_element_by_class_name('CampCatSplashPage_Title__1VpUg').text
+      assert text == 'Campus Catalog'
+   
+   def testAbout(self):
+      self.driver.get(URL)
+      self.driver.implicitly_wait(10)
+      self.driver.find_element_by_link_text('About').click()
+      text = self.driver.find_element_by_class_name('AboutPage_Section__1c-dF').text
+      assert text == 'Our Mission'
+
+      self.driver.back()
+      currentURL = self.driver.current_url
+      assert currentURL == URL
+
+   def testHousing(self):
+      self.driver.get(URL)
+      self.driver.implicitly_wait(10)
+      self.driver.find_element_by_link_text('Housing').click()
+      self.driver.implicitly_wait(30)
+      text = self.driver.find_element_by_tag_name('h1').text
+      assert text == 'Housing'
+
+      self.driver.back()
+      currentURL = self.driver.current_url
+      assert currentURL == URL
+   
+   def testAmenities(self):
+      self.driver.get(URL)
+      self.driver.implicitly_wait(10)
+      self.driver.find_element_by_link_text('Amenities').click()
+      self.driver.implicitly_wait(30)
+      text = self.driver.find_element_by_tag_name('h1').text
+      assert text == 'Amenities'
+
+      self.driver.back()
+      currentURL = self.driver.current_url
+      assert currentURL == URL
+
+   def testUniversities(self):
+      self.driver.get(URL)
+      self.driver.implicitly_wait(10)
+      self.driver.find_element_by_link_text('Universities').click()
+      self.driver.implicitly_wait(30)
+      text = self.driver.find_element_by_tag_name('h1').text
+      assert text == 'Universities'
+
+      self.driver.back()
+      currentURL = self.driver.current_url
+      assert currentURL == URL
+
+
+if __name__ == "__main__":
+    PATH = sys.argv[1]
+    unittest.main(argv=['first-arg-is-ignored'])
+
