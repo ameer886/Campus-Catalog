723596b001502a1d432cf5659bc6cb2c513944ca
Brandon Hinh <brandonhinh@utexas.edu>
2021-10-25 18:22:26 -0500

    Fix pathing for tests

--- front-end/guitests.py
+++ front-end/guitests.py
@@ -4,15 +4,15 @@ from sys import platform
 if __name__ == "__main__":
    # Use chromedriver based on OS
    if platform == "win32":
-      PATH = "./src/gui_tests/chromedriver.exe"
+      PATH = "front-end/src/gui_tests/chromedriver.exe"
    elif platform == "linux":
-      PATH = "./src/gui_tests/chromedriver_linux"
+      PATH = "front-end/src/gui_tests/chromedriver_linux"
    else:
       print("Unsupported OS")
       exit(-1)
 
-   os.system("python3 ./src/gui_tests/navbar_tests.py " + PATH)
-   os.system("python3 ./src/gui_tests/about_tests.py " + PATH)
-   os.system("python3 ./src/gui_tests/amenities_tests.py " + PATH)
-   os.system("python3 ./src/gui_tests/housing_tests.py " + PATH)
-   os.system("python3 ./src/gui_tests/universities_tests.py " + PATH)
+   os.system("python3 front-end/src/gui_tests/navbar_tests.py " + PATH)
+   os.system("python3 front-end/src/gui_tests/about_tests.py " + PATH)
+   os.system("python3 front-end/src/gui_tests/amenities_tests.py " + PATH)
+   os.system("python3 front-end/src/gui_tests/housing_tests.py " + PATH)
+   os.system("python3 front-end/src/gui_tests/universities_tests.py " + PATH)
