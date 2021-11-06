7fd2a5a32f5c50e9e43e658f10e447617c9a67b3
RG8452 <ryangahagan18@gmail.com>
2021-11-06 14:18:14 -0500

    Stylize for input boxes with example

--- front-end/src/components/FilterPopover/FilterPopover.tsx
+++ front-end/src/components/FilterPopover/FilterPopover.tsx
@@ -8,24 +8,46 @@ import { BsFilterCircle } from 'react-icons/bs';
 import styles from './FilterPopover.module.css';
 
 const FilterPopover: React.FunctionComponent = () => {
-  const pop = (
-    <Popover id="popover-basic">
-      <Popover.Title as="h3">Filter Options</Popover.Title>
-      <Popover.Content>
-        <p>You found me!</p>
+  const inputArray = [
+    { value: '1', displayStr: 'Option 1' },
+    { value: '2', displayStr: 'Option 2' },
+    { value: '3', displayStr: 'Option 3' },
+  ];
 
-        {/*
-         * This is a very unobvious way to close the popover
-         * We make use of the rootClose parameter of the
-         * OverlayTrigger and simulate a click onto the document
-         * This click hits nothing but still closes the popover
-         */}
-        <Button
-          aria-label="Close"
-          onClick={() => document.body.click()}
-        >
-          Close
-        </Button>
+  const pop = (
+    <Popover id="popover-basic" style={{ minWidth: '360px' }}>
+      <Popover.Title>
+        <div className={styles.TitleSplitter}>
+          <h3>Filter Options</h3>
+
+          {/*
+           * This is a very unobvious way to close the popover
+           * We make use of the rootClose parameter of the
+           * OverlayTrigger and simulate a click onto the document
+           * This click hits nothing but still closes the popover
+           */}
+          <Button
+            aria-label="Close"
+            onClick={() => document.body.click()}
+          >
+            Apply
+          </Button>
+        </div>
+      </Popover.Title>
+      <Popover.Content>
+        <p>Select one of the following:</p>
+
+        {inputArray.map((item, index) => (
+          <div key={index} className={styles.InputDiv}>
+            <input
+              className={styles.Input}
+              type="checkbox"
+              value={item.value}
+              id={item.value}
+            />
+            <label htmlFor={item.value}>{item.displayStr}</label>
+          </div>
+        ))}
       </Popover.Content>
     </Popover>
   );
