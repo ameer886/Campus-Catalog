29d55b37ec7cc1bd0aad3f3162ce50f3c0afe0b1
Brandon Hinh <brandonhinh@utexas.edu>
2021-11-15 21:56:23 -0600

    Fix pagination_header references

--- back-end/main.py
+++ back-end/main.py
@@ -209,7 +209,6 @@ all_housing_schema = HousingSchema(only=table_columns, many=True)
 
 @app.route('/search', methods=['GET'])
 def search():
-    # TODO: extend to other models
     models = request.args.get('models', default=['Housing', 'Amenities', 'University'], type=lambda v: v.split(','))
     models = [model.capitalize() for model in models]
     query_terms = request.args.get('q', default=[], type=lambda v: v.split(' '))
@@ -230,7 +229,7 @@ def search():
     amenities_page = request.args.get('amenities_page', default=1, type=int)
     amenities_per_page = request.args.get('amenities_per_page', default=10, type=int)
     amenities = {}
-    amenities_paginated_response = {}
+    amenities_pagination_header = {}
     if 'Amenities' in models:
         amenities_query = search_amenities(query_terms)
         amenities_paginated_response = amenities_query.paginate(amenities_page, max_per_page=amenities_per_page)
@@ -243,7 +242,7 @@ def search():
     universities_page = request.args.get('universities_page', default=1, type=int)
     universities_per_page = request.args.get('universities_per_page', default = 10, type = int)
     universities = {}
-    univ_paginated_response = {}
+    univ_pagination_header = {}
     if 'University' in models:
         univ_paginated_response = search_universities(query_terms).paginate(universities_page, max_per_page=universities_per_page)
         universities = {"universities": all_univ_schema.dump(univ_paginated_response.items)}
