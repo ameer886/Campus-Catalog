f76a11f3f56201448e8c987257589b8c5aa8b046
RG8452 <ryangahagan18@gmail.com>
2021-11-24 15:00:59 -0600

    Rework PaginatedTable

--- front-end/src/components/Pagination/PaginatedTable.tsx
+++ front-end/src/components/Pagination/PaginatedTable.tsx
@@ -3,11 +3,12 @@ import { useState } from 'react';
 
 import styles from './PaginatedTable.module.css';
 
-import GenericTable from '../GenericTable/GenericTable';
 import type {
-  GenericTableProps,
+  ColumnDefinitionType,
   RowWithIndex,
 } from '../GenericTable/GenericTable';
+import type { IntentionallyAny } from '../../utilities';
+import QueryTable from './QueryTable';
 import PaginationRelay from './PaginationRelay';
 
 export const PAGE_SIZE = 10;
@@ -19,29 +20,52 @@ export type PaginationMeta = {
   total_items: number;
 };
 
+type PaginationTableProps<
+  T extends RowWithIndex,
+  K extends keyof T,
+> = {
+  params: string;
+  processResponse: (data: IntentionallyAny) => T[];
+
+  // query table props that must be inherited but are unused here
+  model: string;
+  columnDefinitions: Array<ColumnDefinitionType<T, K>>;
+  parentSort?: React.Dispatch<React.SetStateAction<string>>;
+  parentStr?: string;
+};
+
 /*
- * Wrapper class for the generic table that also adds pagination
- * Pages are currently set to be 10 rows long
- * You should be able to at least move forward and backward one page
+ * Wrapper around QueryTable which also provides pagination
+ * into the query
  */
 const PaginatedTable = <T extends RowWithIndex, K extends keyof T>({
-  columnDefinitions,
-  data,
-}: GenericTableProps<T, K>): JSX.Element => {
+  params,
+  processResponse,
+  ...rest
+}: PaginationTableProps<T, K>): JSX.Element => {
   const [page, setPage] = useState(0);
+  const [totalItems, setTotalItems] = useState(0);
 
+  const processPage = (data: IntentionallyAny) => {
+    const responseMeta: PaginationMeta = { ...data[0] };
+    setTotalItems(responseMeta.total_items);
+    return processResponse(data[1]);
+  };
+
+  const pageParams = `page=${page}&per_page=${PAGE_SIZE}` + params;
   return (
     <div className={styles.PaginatedContainer}>
-      <GenericTable
-        columnDefinitions={columnDefinitions}
-        data={data}
+      <QueryTable
+        params={pageParams}
+        processResponse={processPage}
+        {...rest}
       />
 
       <PaginationRelay
         curPage={page}
         setPage={setPage}
         pageSize={PAGE_SIZE}
-        totalElements={data.length}
+        totalElements={totalItems}
       />
     </div>
   );
