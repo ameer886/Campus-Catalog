c7bdefb0900eb70db47f2472e6ae905ea0fa64b3
RG8452 <ryangahagan18@gmail.com>
2021-11-29 20:51:02 -0600

    Fully implement sunburst, but it's too slow

--- front-end/src/views/Visualization/ProviderViz/ProviderSunburst.tsx
+++ front-end/src/views/Visualization/ProviderViz/ProviderSunburst.tsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 
 import type { IntentionallyAny } from '../../../utilities';
 
+import Sunburst from 'react-d3-zoomable-sunburst';
+
 const ProviderSunburst: React.FunctionComponent = () => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState<IntentionallyAny>(null);
@@ -16,9 +18,9 @@ const ProviderSunburst: React.FunctionComponent = () => {
       const courseData = (await courseFetch.json()).courses;
 
       // Maps prof names to prof objects to group courses
-      const profToCourses = new Map();
+      const profToCourses = new Map<string, IntentionallyAny>();
       // Maps dept names to dept objects to group profs
-      const deptToProfs = new Map();
+      const deptToProfs = new Map<string, IntentionallyAny>();
 
       // For each course
       courseData.forEach((course) => {
@@ -26,36 +28,36 @@ const ProviderSunburst: React.FunctionComponent = () => {
         course.professors.forEach((prof) => {
           if (prof !== 'NaN') {
             // If the professor has no datum yet, make the datum
-            if (!profToCourses.has(prof))
+            if (!profToCourses.has(prof)) {
               profToCourses.set(prof, {
                 name: prof,
                 children: [],
               });
+            }
 
-            const profObj = profToCourses.get(prof);
             // Add the course to the professor
+            const profObj = profToCourses.get(prof);
             profObj.children.push({
+              // note: we're skipping sections here because there's way too much
               name: course['num name'],
-              // Children of a course is every section
-              children: course.section.map((s) => {
-                return {
-                  name: s,
-                  children: [],
-                  value: 1,
-                };
-              }),
+              children: [],
+              value: course.section.length,
             });
 
             // Finally, if the department of this course/prof doesn't exist, add one
-            if (!deptToProfs.has(course['dept ganeral']))
+            if (!deptToProfs.has(course['dept general'])) {
               deptToProfs.set(course['dept general'], {
                 name: course['dept general'],
                 children: [],
               });
+            }
             const deptObj = deptToProfs.get(course['dept general']);
             // Add professor to department if we haven't already done so
-            if (!deptObj.children.includes(profObj))
-              deptObj.children.push(profObj);
+            let includesProf = false;
+            for (let i = 0; i < deptObj.children.length; i++)
+              if (deptObj.children[i].name === profObj.name)
+                includesProf = true;
+            if (!includesProf) deptObj.children.push(profObj);
           }
         });
       });
@@ -64,16 +66,34 @@ const ProviderSunburst: React.FunctionComponent = () => {
         name: 'Bevos',
         children: Array.from(deptToProfs.values()),
       };
-      console.log(sunburstData);
       setData(sunburstData);
       setLoading(false);
     };
     fetchDataAsync();
   }, []);
 
-  if (loading) return <p>Loading</p>;
+  if (loading || data == null) return <p>Loading</p>;
 
-  return <div />;
+  return (
+    <div
+      style={{
+        justifyContent: 'center',
+        display: 'flex',
+      }}
+    >
+      <Sunburst
+        data={data}
+        scale="exponential"
+        tooltipContent={<div className="sunburstTooltip" />}
+        tooltip
+        tooltipPosition="right"
+        keyId="Sunburst"
+        value="value"
+        width={window.innerWidth * 0.8}
+        height={window.innerHeight * 0.8}
+      />
+    </div>
+  );
 };
 
 export default ProviderSunburst;
