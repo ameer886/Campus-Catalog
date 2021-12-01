895bb3109bf8211a223413e638faa169f3c82358
RG8452 <ryangahagan18@gmail.com>
2021-12-01 00:49:35 -0600

    Downgraded react to 16.14

--- front-end/src/views/Visualization/OurViz/VizStack.tsx
+++ front-end/src/views/Visualization/OurViz/VizStack.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import UnivCostChart from './BubbleVisualization'
+import UnivCostChart from './BubbleVisualization';
 const VizStack: React.FunctionComponent = () => {
   return (
     <>
       <h2>Our Visualizations</h2>
       <p>This will eventually be our three viz components.</p>
-      < UnivCostChart />
+      <UnivCostChart />
     </>
   );
 };
